88406fa072948f5d4d6e49bcb319e883d9552192
PratikZajam <zajampratik@gmail.com>
2025-01-19 14:52:45 +0530

    added data

--- src/components/Header/Head.jsx
+++ src/components/Header/Head.jsx
@@ -1,49 +1,44 @@
 import React from "react";
-import { useUser } from "../../context/UserContext";
-
-const storedUser = localStorage.getItem("user");
-const user = JSON.parse(storedUser);
-
 
 export const Head = () => {
- 
-
   return (
-    <section className="head">
-      <div className="container d_flex">
-        <div className="left-row">
-          <a
-            target="_blank"
-            href="https://api.whatsapp.com/send?phone=8801317089432"
-            className="icon-flex phone-icon"
-          >
-            <i className="fa fa-phone"></i>
-            <label className="phone-icon" htmlFor="">
-              {user.name} {/* Access user name here */}
-            </label>
-          </a>
-          <a
-            target="_blank"
-            href="mailto:arshadchowdhury46@gmail.com"
-            className="icon-flex phone-icon"
-          >
-            <i className="fa fa-envelope"></i>
-            <label className="phone-icon" htmlFor="">
-              zajampratik@gmail.com
-            </label>
-          </a>
+    <>
+      <section className="head">
+        <div className="container d_flex">
+          <div className="left-row">
+            <a
+              target="_blank"
+              href="https://api.whatsapp.com/send?phone=8801317089432"
+              className="icon-flex phone-icon"
+            >
+              <i className="fa fa-phone"></i>
+              <label className="phone-icon" htmlFor="">
+                +919082684754
+              </label>
+            </a>
+            <a
+              target="_blank"
+              href="mailto:arshadchowdhury46@gmail.com"
+              className="icon-flex phone-icon"
+            >
+              <i className="fa fa-envelope"></i>
+              <label className="phone-icon" htmlFor="">
+                zajampratik@gmail.com
+              </label>
+            </a>
+          </div>
+          <div className="right-row">
+            <span>
+              <label htmlFor="">Need Help ?</label>
+            </span>
+            <span>
+              <span>🇧🇩 </span>
+              <label htmlFor="">EN</label>
+            </span>
+          </div>
         </div>
-        <div className="right-row">
-          <span>
-            <label htmlFor="">Need Help ?</label>
-          </span>
-          <span>
-            <span>🇧🇩 </span>
-            <label htmlFor="">EN</label>
-          </span>
-        </div>
-      </div>
-    </section>
+      </section>
+    </>
   );
 };
 
